bec7ea6f1b19c65d13a20f6533676ff2ff19b12e
divesh koli <kolidivesh05@gmail.com>
2025-08-14 16:18:53 +0530

    Updated settings for Render deployment

--- dressrosa/settings.py
+++ dressrosa/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,3 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'myapp', 'media')
 # DEFAULT PRIMARY KEY FIELD TYPE
 # -----------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
