b0c5c9bbecaa8d8f8d638e024f125126b94fcdb2
Divesh/harsh <kolidivesh05@hmail.com>
2026-05-13 00:23:56 +0530

    fixed media files

--- dressrosa/settings.py
+++ dressrosa/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Additional locations for static files during development
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'myapp', 'static'),
 ]
 
 # -----------------------------
--- dressrosa/urls.py
+++ dressrosa/urls.py
@@ -16,13 +16,12 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path,include
-
-from django.conf.urls.static import static
-
+from django.urls import path, include, re_path
+from django.views.static import serve
 from . import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("myapp.urls")),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('', include("myapp.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
