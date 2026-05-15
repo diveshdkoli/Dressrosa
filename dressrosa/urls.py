e15f35fd4b9f58c29e57d63a675dc3aeef33ebe3
Divesh/harsh <kolidivesh05@hmail.com>
2026-05-15 10:52:43 +0530

    fixed media files

--- dressrosa/urls.py
+++ dressrosa/urls.py
@@ -18,20 +18,26 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
-from . import view
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
+
 from myapp.sitemaps import StaticViewSitemap
 
 sitemaps = {
     'static': StaticViewSitemap,
-
 }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include("myapp.urls")),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+
     path(
         "robots.txt",
         TemplateView.as_view(
--- myapp/sitemaps.py
+++ myapp/sitemaps.py
@@ -1,18 +1,23 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
-class StaticViewSitemap(Sitemap):
 
+class StaticViewSitemap(Sitemap):
     priority = 0.8
     changefreq = 'weekly'
 
     def items(self):
         return [
-            'home',
+            'index',
             'men',
             'women',
             'kids',
             'contact',
+            'register',
+            'login_page',
+            'profile',
+            'product_list',
+            'cart_page',
         ]
 
     def location(self, item):
